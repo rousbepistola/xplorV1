8a729a719d621d34ed08c46736461f57b43d072d
rousbepistola <56054397+rousbepistola@users.noreply.github.com>
2020-01-07 21:27:49 -0500

    Agree to proceed added. maybe add design?

--- app/src/main/java/com/example/xplorv1/Main3Activity.java
+++ app/src/main/java/com/example/xplorv1/Main3Activity.java
@@ -2,7 +2,17 @@ package com.example.xplorv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+//add:
+//how to play
+//about
+//contact us
+
+//make roux studios show a web view of my web site
+
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -10,5 +20,57 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        getSupportActionBar().hide();
+    }
+
+
+                         //DASHBOARD AREA
+    public void howtoplay(View view){
+        Intent ni = new Intent(this, howtoplay.class);
+        startActivity(ni);
+    }
+
+    public void aboutUs(View view){
+        Intent ni = new Intent(this, aboutUs.class);
+        startActivity(ni);
+    }
+
+    public void contactUs(View view){
+        Intent ni = new Intent(this, contactUs.class);
+        startActivity(ni);
+    }
+
+
+
+                            //XPLOR AREA
+    public void xplrone(View view){
+        Intent ni = new Intent(this, xplrone.class);
+        startActivity(ni);
+    }
+
+    public void xplrtwo(View view){
+        Intent ni = new Intent(this, xplrtwo.class);
+        startActivity(ni);
+    }
+
+    public void xplrthree(View view){
+        Intent ni = new Intent(this, xplrthree.class);
+        startActivity(ni);
+    }
+
+    public void xplrfour(View view){
+        Intent ni = new Intent(this, xplrfour.class);
+        startActivity(ni);
+    }
+
+    public void xplrfive(View view){
+        Intent ni = new Intent(this, xplrfive.class);
+        startActivity(ni);
+    }
+
+    public void xplrsix(View view){
+        Intent ni = new Intent(this, xplrsix.class);
+        startActivity(ni);
     }
 }
