ac3a60f532945370a881a7d1d85b0de79e187f50
rousbepistola <56054397+rousbepistola@users.noreply.github.com>
2020-01-08 21:41:03 -0500

    Agree to proceed added. maybe add design?

--- app/src/main/java/com/example/xplorv1/aboutUs.java
+++ app/src/main/java/com/example/xplorv1/aboutUs.java
@@ -10,5 +10,6 @@ public class aboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        getSupportActionBar().hide();
     }
 }
--- app/src/main/java/com/example/xplorv1/contactUs.java
+++ app/src/main/java/com/example/xplorv1/contactUs.java
@@ -2,13 +2,57 @@ package com.example.xplorv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class contactUs extends AppCompatActivity {
+    EditText fname, subject, email, message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+        getSupportActionBar().hide();
+
+        fname = findViewById(R.id.fname);
+        subject = findViewById(R.id.subject);
+        email = findViewById(R.id.email);
+        message = findViewById(R.id.message);
+    }
+
+
+
+
+    public void submit(View v){
+        String name = String.valueOf(fname);
+        String title = String.valueOf(subject);
+        String messages = String.valueOf(message);
+
+        Intent msg = new Intent(Intent.ACTION_SEND);
+        msg.setType("message/rfc822");
+        msg.putExtra(Intent.EXTRA_EMAIL , new String[]{"rous.epistola@triosstudent.com"});
+        msg.putExtra(Intent.EXTRA_SUBJECT, "Email From: " + name + " Subject: " + title);
+        msg.putExtra(Intent.EXTRA_TEXT, messages);
+
+        try{
+            startActivity(Intent.createChooser(msg, "Send mail..."));
+        } catch(android.content.ActivityNotFoundException ex){
+            Toast.makeText(this, "No email client installed",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+
+    public void call(View v){
+
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:+16475273835"));//change the number
+        startActivity(callIntent);
+
     }
 }
